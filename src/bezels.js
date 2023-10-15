81e336041961a6759316c2d1eddf93f00fdfc647
RadekVym <rvymetalik@email.cz>
2023-10-15 22:19:50 +0200

    Added new bezels

--- src/bezels.js
+++ src/bezels.js
@@ -8,13 +8,40 @@ export const bezels = {
         height: 4297,
         contentScale: 0.956
     },
-    android: {
-        key: 'android_bezel',
-        title: 'Android (19.5:9)',
-        image: 'images/bezels/android_bezel.png',
-        mask: 'images/bezels/android_bezel_mask.png',
+    'android_gold_19,5_9': {
+        key: 'android_gold_19,5_9',
+        title: 'Android Gold (19.5:9)',
+        image: 'images/bezels/android_gold_19,5_9.png',
+        mask: 'images/bezels/android_gold_19,5_9_mask.png',
         width: 620,
         height: 1300,
         contentScale: 0.968
+    },
+    'android_gray_19,5_9': {
+        key: 'android_gray_19,5_9',
+        title: 'Android Gray (19.5:9)',
+        image: 'images/bezels/android_gray_19,5_9.png',
+        mask: 'images/bezels/android_gray_19,5_9_mask.png',
+        width: 932,
+        height: 1920,
+        contentScale: 0.958
+    },
+    'android_gray_20_9': {
+        key: 'android_gray_20_9',
+        title: 'Android Gray (20:9)',
+        image: 'images/bezels/android_gray_20_9.png',
+        mask: 'images/bezels/android_gray_20_9_mask.png',
+        width: 911,
+        height: 1920,
+        contentScale: 0.960
+    },
+    'android_gray_21_9': {
+        key: 'android_gray_21_9',
+        title: 'Android Gray (21:9)',
+        image: 'images/bezels/android_gray_21_9.png',
+        mask: 'images/bezels/android_gray_21_9_mask.png',
+        width: 872,
+        height: 1920,
+        contentScale: 0.970
     },
 };
